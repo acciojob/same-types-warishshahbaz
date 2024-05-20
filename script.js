96de517ed0792385f09cf7d954d0e3f444f3eb2b
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-05-20 14:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ function isSameType(value1, value2) {
         return true;
     }
     
-    // Check if types of both values are the same
+    // Check if the types of both values are the same
     return typeof value1 === typeof value2;
 }
 
-// do not change the code below.
-//let value1 = prompt("Enter Start of the Range.");
-//let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+// Test cases
+console.log(isSameType(1, 3));        // true
+console.log(isSameType("hey", "hello"));   // true
+console.log(isSameType(NaN, NaN));    // true
+console.log(isSameType("3", 3));
